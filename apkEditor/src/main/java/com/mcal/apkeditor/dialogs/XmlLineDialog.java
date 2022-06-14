bd6b691d9d508b24747240d818574670c4069b72
Иван Тимашков <timscriptov@mail.ru>
2022-06-14 19:03:00 +0300

    ...

--- apkEditor/src/main/java/com/mcal/apkeditor/dialogs/XmlLineDialog.java
+++ apkEditor/src/main/java/com/mcal/apkeditor/dialogs/XmlLineDialog.java
@@ -8,16 +8,15 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.widget.AppCompatEditText;
-import androidx.appcompat.widget.AppCompatTextView;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.mcal.apkeditor.R;
 
 import java.util.ArrayList;
@@ -75,6 +74,7 @@ public class XmlLineDialog {
         View view = LayoutInflater.from(context).inflate(R.layout.dlg_xmlline, null);
 
         AlertDialog materialDialog = new MaterialAlertDialogBuilder(context)
+                .setTitle("Manifest Editor")
                 .setView(view)
                 .create();
 
@@ -92,9 +92,9 @@ public class XmlLineDialog {
             int index = 0;
             for (Entry<String, String> entry : keyValues.entrySet()) {
                 View child = LayoutInflater.from(context).inflate(R.layout.item_stringvalue, null);
-                AppCompatTextView tv = child.findViewById(R.id.string_name);
-                tv.setText(entry.getKey());
-                AppCompatEditText valueEt = child.findViewById(R.id.string_value);
+                TextInputLayout tv = child.findViewById(R.id.string_name);
+                tv.setHint(entry.getKey());
+                TextInputEditText valueEt = child.findViewById(R.id.string_value);
                 valueEtList.add(valueEt);
                 valueEt.setText(entry.getValue());
                 keyValueLayout.addView(child, index++);
@@ -138,9 +138,9 @@ public class XmlLineDialog {
                     keyValues.put(strKey, strValue);
 
                     View child = LayoutInflater.from(context).inflate(R.layout.item_stringvalue, null);
-                    TextView tv = child.findViewById(R.id.string_name);
-                    tv.setText(strKey);
-                    EditText valueEdit = child.findViewById(R.id.string_value);
+                    TextInputLayout tv = child.findViewById(R.id.string_name);
+                    tv.setHint(strKey);
+                    TextInputEditText valueEdit = child.findViewById(R.id.string_value);
                     valueEtList.add(valueEdit);
                     valueEdit.setText(strValue);
                     keyValueLayout.addView(child, keyValues.size() - 1);
