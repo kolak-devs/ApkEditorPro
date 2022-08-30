2e0552314f2ffa7eb5c2ff4cacd03d46f7cd2481
SVolf <aftervolf@gmail.com>
2022-08-31 00:33:08 +0700

    Some improvements

--- melissa/src/main/java/ru/svolf/melissa/swipeback/SwipeBackActivity.java
+++ melissa/src/main/java/ru/svolf/melissa/swipeback/SwipeBackActivity.java
@@ -26,6 +26,7 @@ public class SwipeBackActivity extends CustomizedLangActivity {
         super.onCreate(savedInstanceState);
 
         onActivityCreate();
+        setEdgeLevel(SwipeBackLayout.EdgeLevel.MED);
     }
 
     @Override
