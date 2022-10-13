1b68854a4a156a79fc4671f455382be0b5bf9e6d
SVolf <aftervolf@gmail.com>
2022-10-13 09:57:33 +0700

    test

--- features/selectapk/src/main/java/ru/svolf/feature_selectapk/FileManagerViewModel.kt
+++
@@ -1,7 +0,0 @@
-package ru.svolf.feature_selectapk
-
-import androidx.lifecycle.ViewModel
-
-class FileManagerViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
---
+++ features/selectapk/src/main/java/ru/svolf/feature_selectapk/ui/FileViewModel.kt
@@ -0,0 +1,7 @@
+package ru.svolf.feature_selectapk.ui
+
+import androidx.lifecycle.ViewModel
+
+class FileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
