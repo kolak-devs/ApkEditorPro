b5d39e31fe5bc011f67a1f28f324ec5840ab31a4
SVolf <aftervolf@gmail.com>
2022-10-23 22:48:33 +0700

    Revert "Update FileListActivity.kt"
    This reverts commit 47bce5fc18dd6b35391662716c950f4ef591d039.

--- apkEditor/src/main/java/com/mcal/apkeditor/activities/FileListActivity.kt
+++ apkEditor/src/main/java/com/mcal/apkeditor/activities/FileListActivity.kt
@@ -25,8 +25,11 @@ import com.mcal.apkeditor.ui.fulleditor.FullEditorActivity
 import com.mcal.apksigner.ApkSigner
 import com.mcal.common.activities.CustomizedLangActivity
 import com.mcal.common.data.Preferences
-import com.mcal.common.utils.*
+import com.mcal.common.utils.ActivityHelper
+import com.mcal.common.utils.ApkInfoParser
+import com.mcal.common.utils.FileRecord
 import com.mcal.common.utils.ScopedStorage.storageDirectory
+import com.mcal.common.utils.findExt
 import com.mcal.common.view.ProgressDialog
 import com.mcal.editor.TextEditor.getSoraEditor
 import com.mcal.folderlist.FolderListWrapper
@@ -293,9 +296,7 @@ class FileListActivity : CustomizedLangActivity(), IListEventListener, IListItem
                 @Throws(Exception::class)
                 override fun process() {
                     CoroutineScope(Dispatchers.IO).launch {
-                        if (ScopedStorage.getKey() != null) {
-                            ApkSigner().signApkCustom(unsignedPath, signedPath)
-                        } else Toast.makeText(this@FileListActivity, "Please set-up keystore params in settings!", Toast.LENGTH_SHORT).show()
+                        ApkSigner().signApkCustom(unsignedPath, signedPath)
                     }
                 }
 
