7e58a3215a06db0407911c01bf2cba34cd8f781a
Иван Тимашков <timscriptov@mail.ru>
2023-01-03 03:36:14 +0500

    Update DownloaderAdapter.kt
    Возможно пофиксил

--- net-manager/src/main/java/com/mcal/downloader/DownloaderAdapter.kt
+++ net-manager/src/main/java/com/mcal/downloader/DownloaderAdapter.kt
@@ -17,7 +17,7 @@ import kotlinx.coroutines.launch
 import java.io.File
 
 class DownloaderAdapter(
-    private val tools: MutableList<Pair<String, String>>,
+    private val tools: MutableList<Pair<String, String>?>?,
 ) :
     RecyclerView.Adapter<DownloaderAdapter.AppListViewHolder>() {
     override fun onCreateViewHolder(
@@ -30,59 +30,60 @@ class DownloaderAdapter(
     }
 
     override fun onBindViewHolder(holder: AppListViewHolder, position: Int) {
-        val item = tools[position]
-        val name = item.first
-        val url = item.second
-        holder.name.text = name
-        holder.url.text = url
+        tools?.get(position)?.let { item ->
+            val name = item.first
+            val url = item.second
+            holder.name.text = name
+            holder.url.text = url
 
-        val container = holder.container
-        val path = File(ScopedStorage.getBinDir().path + File.separator + name)
-        val buttonDownload = holder.download
-        val context = buttonDownload.context
-        var icon: Drawable?
-        val isFileExists = path.exists()
-        if (isFileExists) {
-            icon = ResourcesCompat.getDrawable(
-                context.resources,
-                R.drawable.ic_delete,
-                context.theme
-            )
-            buttonDownload.setImageDrawable(icon)
-        } else {
-            icon = ResourcesCompat.getDrawable(
-                context.resources,
-                R.drawable.ic_download,
-                context.theme
-            )
-            buttonDownload.setImageDrawable(icon)
-        }
-        buttonDownload.setOnClickListener {
+            val container = holder.container
+            val path = File(ScopedStorage.getBinDir().path + File.separator + name)
+            val buttonDownload = holder.download
+            val context = buttonDownload.context
+            var icon: Drawable?
+            val isFileExists = path.exists()
             if (isFileExists) {
-                if (path.delete()) {
-                    icon = ResourcesCompat.getDrawable(
-                        context.resources,
-                        R.drawable.ic_download,
-                        context.theme
-                    )
-                    buttonDownload.setImageDrawable(icon)
-                    buttonDownload.isEnabled = true
-                }
+                icon = ResourcesCompat.getDrawable(
+                    context.resources,
+                    R.drawable.ic_delete,
+                    context.theme
+                )
+                buttonDownload.setImageDrawable(icon)
             } else {
-                buttonDownload.isEnabled = false
-                CoroutineScope(Dispatchers.IO).launch {
-                    NetHelper.download(
-                        url,
-                        path,
-                        container
-                    )
+                icon = ResourcesCompat.getDrawable(
+                    context.resources,
+                    R.drawable.ic_download,
+                    context.theme
+                )
+                buttonDownload.setImageDrawable(icon)
+            }
+            buttonDownload.setOnClickListener {
+                if (isFileExists) {
+                    if (path.delete()) {
+                        icon = ResourcesCompat.getDrawable(
+                            context.resources,
+                            R.drawable.ic_download,
+                            context.theme
+                        )
+                        buttonDownload.setImageDrawable(icon)
+                        buttonDownload.isEnabled = true
+                    }
+                } else {
+                    buttonDownload.isEnabled = false
+                    CoroutineScope(Dispatchers.IO).launch {
+                        NetHelper.download(
+                            url,
+                            path,
+                            container
+                        )
+                    }
                 }
             }
         }
     }
 
     override fun getItemCount(): Int {
-        return tools.size
+        return tools?.size ?: 0
     }
 
     class AppListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
