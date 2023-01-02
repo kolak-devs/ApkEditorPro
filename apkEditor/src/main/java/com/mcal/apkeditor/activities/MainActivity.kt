140f18b3d7e6bba17251529344fee7ea1bb99306
SVolf <aftervolf@gmail.com>
2023-01-02 19:34:16 +0700

    Button style

--- apkEditor/src/main/java/com/mcal/apkeditor/activities/MainActivity.kt
+++ apkEditor/src/main/java/com/mcal/apkeditor/activities/MainActivity.kt
@@ -90,7 +90,6 @@ class MainActivity : CustomizedLangActivity(), ProcessingInterface {
                                 delegate.applyDayNight()
                             }
                         }
-
                         return true
                     }
                 }
