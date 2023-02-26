f1c402032684300ac0477503a62f4ebf39b323d3
Иван Тимашков <timscriptov@mail.ru>
2023-02-26 23:20:12 +0500

    Update XString.kt

--- feature/bsh-engine/src/main/java/com/mcal/bshengine/api/XString.kt
+++ feature/bsh-engine/src/main/java/com/mcal/bshengine/api/XString.kt
@@ -10,4 +10,46 @@ class XString {
     fun unescapeUnicode(string: String): String = string.replace("\\\\u([\\dA-Fa-f]{4})".toRegex()) {
         String(Character.toChars(it.groupValues[1].toInt(radix = 16)))
     }
+
+    /**
+     * Since: 2.4.6
+     */
+    fun isNotEmpty(string: String): Boolean {
+        return string.isNotEmpty()
+    }
+
+    /**
+     * Since: 2.4.6
+     */
+    fun isEmpty(string: String): Boolean {
+        return string.isEmpty()
+    }
+
+    /**
+     * Since: 2.4.6
+     */
+    fun isNullOrEmpty(string: String?): Boolean {
+        return string.isNullOrEmpty()
+    }
+
+    /**
+     * Since: 2.4.6
+     */
+    fun isNotBlank(string: String): Boolean {
+        return string.isNotBlank()
+    }
+
+    /**
+     * Since: 2.4.6
+     */
+    fun isBlank(string: String): Boolean {
+        return string.isBlank()
+    }
+
+    /**
+     * Since: 2.4.6
+     */
+    fun isNullOrBlank(string: String?): Boolean {
+        return string.isNullOrBlank()
+    }
 }
